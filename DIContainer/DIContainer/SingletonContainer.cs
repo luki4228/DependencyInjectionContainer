2cc589df5c4237bd5ab1a32bfdffbf170a2dcea3
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-16 12:59:18 +0300

    added Singleton Container and Dependencies Config

---
+++ DIContainer/DIContainer/SingletonContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    class SingletonContainer
+    {
+        public volatile object instance;
+        public object syncRoot = new object();
+
+        public SingletonContainer()
+        {
+            instance = null;
+        }
+    }
+}
