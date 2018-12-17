782af8b32dac001f50e1c5ef45d41973ce5e748c
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-18 01:49:56 +0300

    amplemented some logic of resolve

--- DIContainer/DIContainer/DIProvider.cs
+++ DIContainer/DIContainer/DIProvider.cs
@@ -12,10 +12,11 @@ namespace DIContainer
 
         public DIProvider(DIConfig _config)
         {
+            validateConfig(_config);
             config = _config;
         }
 
-        public void validateConfiguration(DIConfig _config)
+        public void validateConfig(DIConfig _config)
         {
             foreach (Type depType in config.DContainer.Keys)
             {
@@ -40,10 +41,56 @@ namespace DIContainer
             }
         }
 
-        public TImplementation Resolve<TImplementation>()
+        public List<TDependency> Resolve<TDependency>()
         {
-            throw new NotImplementedException();
+            Type depType = typeof(TDependency);
+            if (depType.IsGenericType && depType.GetGenericTypeDefinition().Equals(typeof(IEnumerable<>)))
+            {
+                depType = depType.GenericTypeArguments[0];
+            }
+
+            if (depType.IsGenericType)
+            {
+                foreach (var type in depType.GenericTypeArguments)
+                {
+                    if (!config.DContainer.ContainsKey(type))
+                    {
+                        throw new KeyNotFoundException($"Dependency {type.ToString()} is not registered.");
+                    }
+                }
+            }
+            else
+            if (!config.DContainer.ContainsKey(depType))
+            {
+                throw new KeyNotFoundException($"Dependency {depType.ToString()} is not registered.");
+            }
+
+            List<TDependency> result = new List<TDependency>();
+
+            
+
+            return result;
+
         }
-    
+
+        public object ResolveT(Type t)
+        {
+            var rMethod = typeof(DIProvider).GetMethod("Resolve");
+            var rType = rMethod.MakeGenericMethod(t);
+
+            var listType = typeof(List<>);
+            var constructedListType = listType.MakeGenericType(t);
+
+            dynamic resolved = Convert.ChangeType(rType.Invoke(this, null), constructedListType);
+
+            if (resolved.Count > 1)
+            {
+                return resolved;
+            }
+            else
+            {
+                return resolved[0];
+            }
+        }
     }
 }
