a40391038d5e47fec353bc2012550d8c07953eed
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-17 22:33:37 +0300

    added validation of config

--- DIContainer/DIContainer/DIProvider.cs
+++ DIContainer/DIContainer/DIProvider.cs
@@ -8,11 +8,36 @@ namespace DIContainer
 {
     public class DIProvider
     {
-        DIConfig config;
+        private DIConfig config;
 
         public DIProvider(DIConfig _config)
         {
             config = _config;
+        }
+
+        public void validateConfiguration(DIConfig _config)
+        {
+            foreach (Type depType in config.DContainer.Keys)
+            {
+                if (depType.IsValueType)
+                {
+                    throw new ArgumentException("TDependency must be a ref type");
+                }
+
+                foreach (Type implemType in config.DContainer[depType])
+                {
+                    if (!depType.IsAssignableFrom(implemType) && !depType.IsGenericTypeDefinition)
+                    {
+                        throw new ArgumentException("TImplementation must implement or be inherited from TDependency.");
+                    }
+
+                    if (implemType.IsAbstract || !implemType.IsClass)
+                    {
+                        throw new ArgumentException("TImplementation must be class(not abstract)");
+                    }
+
+                }
+            }
         }
 
         public TImplementation Resolve<TImplementation>()
