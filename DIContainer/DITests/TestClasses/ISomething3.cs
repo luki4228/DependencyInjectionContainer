5bb320961eb7a39ef9b3cb65a6ef44fae8c9a236
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-18 10:58:47 +0300

    added some test classes and basic tests

---
+++ DIContainer/DITests/TestClasses/ISomething3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITests.TestClasses
+{
+    public interface ISomething3<T> where T : SomethingAbstract1
+    {
+        void DoIt(T t);
+    }
+}
--- DIContainer/DITests/UnitTest1.cs
+++ DIContainer/DITests/UnitTest1.cs
@@ -1,14 +1,91 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIContainer;
+using DITests.TestClasses;
 
 namespace DITests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void InitializeTest()
         {
+
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestReferenceTypeValidation()
+        {
+            var config = new DIConfig();
+            config.Register<int, int>(true);
+            var provider = new DIProvider(config);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestImplementationInheritenceValidation()
+        {
+            var config = new DIConfig();
+            config.Register<String, Boolean>(true);
+            var provider = new DIProvider(config);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestImplementationTypeValidation()
+        {
+            var config = new DIConfig();
+            config.Register<SomethingAbstract1, SomeAbstractImplement>(true);
+            var provider = new DIProvider(config);
+        }
+
+        [TestMethod]
+        public void TestDuplicateImplementations()
+        {
+            var config = new DIConfig();
+            config.Register<SomethingAbstract1, SomeImplementation>(true);
+            config.Register<SomethingAbstract1, SomeImplementation>(true);
+            var provider = new DIProvider(config);
+
+            Assert.AreEqual(config.DContainer[typeof(SomethingAbstract1)].Count, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void TestUnregisteredDependencies()
+        {
+            var config = new DIConfig();
+            var provider = new DIProvider(config);
+            var obj = provider.Resolve<SomethingAbstract1>();
+        }
+
+        [TestMethod]
+        public void TestResolvingBasicImplementation()
+        {
+            var config = new DIConfig();
+            config.Register<SomethingAbstract1, SomeAbstractImplement>(true);
+            var provider = new DIProvider(config);
+            var obj = provider.Resolve<SomethingAbstract1>();
+            Assert.IsInstanceOfType(obj[0], typeof(SomeAbstractImplement));
+        }
+
+        // Check for resolving dependencies that can't be created.
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestResolvingUncreatableDependencies()
+        {
+            var config = new DIConfig();
+            config.Register<SomethingAbstract1, SomethingPrivate>(true);
+            var provider = new DIProvider(config);
+            var obj = provider.Resolve<SomethingAbstract1>();
+        }
+
+
     }
 }
